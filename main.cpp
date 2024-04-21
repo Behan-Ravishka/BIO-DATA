d7ca5044d4f41b6d4dfaba11e4f47ace6b92f6b5
Behan <behanravishka03@gmail.com>
2024-04-21 12:31:59 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "BIO-DATA" << endl;
+    cout << "\n------------------------------------"  << endl;
+    cout << "\nNAME    : Mr. M. P. B. R. PERERA" << endl;
+    cout << "\nADDRESS : No09, Gonagaha, Makewita" << endl;
+    cout << "\nAGE     : 20 years" << endl;
+    cout << "\nEducation Qualification:" << endl;
+    cout << "\n\t G. C. E (A/L) Examination" << endl;
+    cout << "\n\t Mathematics        S" << endl;
+    cout << "\n\t Physics            S" << endl;
+    cout << "\n\t ICT                B" << endl;
+    cout << "\n-------------------------------------" << endl;
+    return 0;
+
+}
